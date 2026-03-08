7334e2d6294c5e4cb76842b7543997cee1621b46
Dan Golant <dgolant@gmail.com>
2026-03-08 01:01:09 -0500

    Remove itsycal
    The macOS widget system does this now I guess

--- macos/defaults-misc.sh
+++ macos/defaults-misc.sh
@@ -1,5 +1,1 @@
 # A folder for configuring miscellaneous apps installed in the Brewfile/Caskfile/Gemfile/npmfile
-
-# Set Itsycal to use the default macOS time format and hide the icon
-defaults write com.mowglii.ItsycalApp HideIcon 1
-defaults write com.mowglii.ItsycalApp ClockFormat "E MMM d h:mm a"
