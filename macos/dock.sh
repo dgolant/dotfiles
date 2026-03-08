ee34bdfd8aa51015f5c496a3831f2cdd4436f8db
Dan Golant <dgolant@gmail.com>
2026-03-08 01:13:29 -0500

    Update dock and add ai deps

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Cron.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/System/Applications/Photos.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Notion.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
 dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Apple Music.app"
 
 killall Dock
